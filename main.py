09ae5eb597db22b13a94b19fd14c8b1b00a25612
Nikhil Dangi <nikhildangi168@gmail.com>
2025-08-31 18:20:13 +0530

    import

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+import os
+import sys
+import random
+import threading
+from datetime import datetime
+
+class Typewriter:
+    def __init__(self):
+        pass
+
+def main():
+    app = Typewriter()
+    app.run()
+
+if __name__ == "__main__":
+    main()
